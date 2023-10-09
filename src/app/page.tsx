37e26d3d2305cd884ef84f616c590236ab4e4f77
ronald-flores-devbase <ronald@ronaldsequeira.com>
2023-10-09 00:28:01 -0600

    Revert "functions and tests"
    This reverts commit 10f80e000db9587b0347678202d7b6c69fffcdcd.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <main>
       <h1>Pert</h1>
+      <Button>Click me</Button>
     </main>
   )
 }
